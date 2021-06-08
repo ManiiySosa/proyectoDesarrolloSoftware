60265c60fcd122261b84d09664b7e72fa8aa36a1
Maniiy <raybanss13@gmail.com>
2021-06-07 19:38:37 -0500

    exception en bd

--- config/db.php
+++ config/db.php
@@ -1,14 +1,21 @@
 <?php
+//error_reporting(0);
 
 class Database{
 	public static function connect(){
-		$db = new mysqli('localhost', 'root', '', 'tienda_camisetas');
-
-		 if (mysqli_connect_error()) {
-            die('Error de Conexión (' . mysqli_connect_errno() . ') '
-                    . mysqli_connect_error());
-         }
+		try{
+			$db = new mysqli('localhost', 'root', '', 'tienda_camisetas');
+			throw new Exception("base de datos sin servicio ");
+		}catch(Exception $e){
+			/*if (mysqli_connect_error()) {
+				die('Error de Conexión (' . mysqli_connect_errno() . ') '
+						. mysqli_connect_error());
+			 }*/
+			 echo $e->getMessage().' <br>'.mysqli_connect_error();
+		}
+		
 		 $db->query("SET NAMES 'utf8'");
+		 $db->close();
 		return $db;
 	}
 }
